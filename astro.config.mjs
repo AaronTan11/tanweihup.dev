d87aa39dfb6feea9713d7644e577e67ab6de2430
AaronTan11 <tanweihup@gmail.com>
2024-06-01 11:47:36 +0800

    feat: added packages

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,17 @@
-import { defineConfig } from "astro/config"
-import tailwind from "@astrojs/tailwind"
+import { defineConfig } from "astro/config";
+import tailwind from "@astrojs/tailwind";
 
-import svelte from "@astrojs/svelte"
+import svelte from "@astrojs/svelte";
+import vue from "@astrojs/vue";
+import mdx from "@astrojs/mdx";
+import solidJs from "@astrojs/solid-js";
 
 // https://astro.build/config
 export default defineConfig({
-	integrations: [tailwind(), svelte()]
-})
+  site: "https://www.nexxel.dev",
+  integrations: [tailwind(), svelte(), mdx(), solidJs(), vue()],
+  prefetch: {
+    prefetchAll: true,
+    defaultStrategy: "viewport",
+  },
+});
