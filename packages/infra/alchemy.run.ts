38e8272360eb93bbf5c45657ad828f468e826250
AaronTan11 <tanweihup@gmail.com>
2026-01-02 17:21:25 +0800

    feat: Configure production environment for infra, set web domains, and update root title.

--- packages/infra/alchemy.run.ts
+++ packages/infra/alchemy.run.ts
@@ -3,9 +3,11 @@ import { TanStackStart } from "alchemy/cloudflare";
 import { config } from "dotenv";
 
 config({ path: "./.env" });
-config({ path: "../../apps/web/.env" });
+config({ path: "../../apps/web/.env.prod" });
 
-const app = await alchemy("tanweihup.dev");
+const app = await alchemy("tanweihup.dev", {
+  profile: "prod"
+});
 
 export const web = await TanStackStart("web", {
   cwd: "../../apps/web",
@@ -13,8 +15,11 @@ export const web = await TanStackStart("web", {
     VITE_CONVEX_URL: alchemy.env.VITE_CONVEX_URL!,
     VITE_CONVEX_SITE_URL: alchemy.env.VITE_CONVEX_SITE_URL!,
   },
+  profile: "prod",
+  domains: ["tanweihup.dev", "www.tanweihup.dev"]
 });
 
 console.log(`Web    -> ${web.url}`);
+console.log(`Web    -> ${web.domains?.map((domain) => domain.name).join(", ")}`);
 
 await app.finalize();
